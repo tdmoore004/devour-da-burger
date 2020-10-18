8c7a245f3c3a5cd712eb3c4f06ef4e9741ea1ffd
Tanner Moore <tdmoore004@gmail.com>
2020-10-17 20:04:10 -0600

    Added functions to orm.js for reading, creating and updating database.

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,55 @@
-const connection = require("./connection");
+const connection = require("./connection.js");
+
+const orm = {
+
+    selectAll: (table, cb) => {
+        const queryString = "SELECT * FROM " + table + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    insertOne: (table, cols, vals, cb) => {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
+    updateOne: (table, objColVals, condition, cb) => {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      }
+};
+
+module.exports = orm;
