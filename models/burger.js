60c44bc433e10eb104c52038db6ae4e0604f3074
Tanner Moore <tdmoore004@gmail.com>
2020-10-17 20:48:59 -0600

    Added models burger.js and called orm functions with burger specific values.

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,24 @@
 const orm = require("./config/orm.js");
+
+const burger = {
+
+    selectAll: (cb) => {
+        orm.all("burgers", (res) => {
+            cb(res);
+        });
+    },
+
+    insertOne: (cols, vals, cb) => {
+        orm.inserOne("burgers", cols, vals, (cb) => {
+            cb(res);
+        });
+    },
+
+    updateOne: (objColVals, condition, cb) => {
+        orm.updateOne("burgers", objColVals, condition, (res) => {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
